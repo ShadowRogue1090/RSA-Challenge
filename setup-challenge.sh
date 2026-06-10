5744d20e11f521c42b42e5d135991d86bcf774e6
ShadowRogue1 <shadowrogue1090@gmail.com>
2026-06-10 18:06:31 +1000

    alice

--- setup-challenge.sh
+++ setup-challenge.sh
@@ -15,8 +15,22 @@ docker build -t hacker ./$HACKER_CONTAINER
 
 ## Create Containers
 
+# User (Alice)
 docker run -d \
   --name "$USER_CONTAINER" \
   --network "$NETWORK" \
   -p 2222:22 \
   user
+
+# Flag (Bob)
+docker run -d \
+  --name "$FLAG_CONTAINER" \
+  --network "$NETWORK" \
+  flag
+
+# Hacker (Hackerbot9001)
+  docker run -d \
+  --name "$HACKER_CONTAINER" \
+  --network "$NETWORK" \
+  -p 2222:22 \
+  hacker
